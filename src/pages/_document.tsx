eb47458f713fa32f29f6a94e62db2071e368442b
Alberto <25537372+albertobas@users.noreply.github.com>
2022-03-01 10:59:53 +0100

    Removed viewport meta tag from _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,10 +9,6 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-          />
           <Favicons />
           <link rel="manifest" href="/manifest.json" />
         </Head>
