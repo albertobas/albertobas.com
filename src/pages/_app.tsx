cd0d4ff3c5a345346c49d23434acdcc52aef05af
Alberto <25537372+albertobas@users.noreply.github.com>
2022-03-01 11:07:23 +0100

    Added some meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,8 @@ export default function Application({ Component, pageProps }: AppProps) {
       <ThemeProvider>
         <MobileProvider>
           <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta
               name="viewport"
               content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
