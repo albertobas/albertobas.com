11cb8865c9bb914be925ec1161100a2710474571
Alberto <25537372+albertobas@users.noreply.github.com>
2022-03-04 16:13:25 +0100

    Edited props interface

--- src/components/utils/IPre.tsx
+++ src/components/utils/IPre.tsx
@@ -1,7 +1,7 @@
 import { useInView } from 'react-intersection-observer';
-import { AnchorHTMLAttributes, DetailedHTMLProps, FC } from 'react';
+import { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
 
-const IPre: FC<DetailedHTMLProps<AnchorHTMLAttributes<HTMLPreElement>, HTMLPreElement>> = (props) => {
+const IPre: FC<DetailedHTMLProps<HTMLAttributes<HTMLPreElement>, HTMLPreElement>> = (props) => {
   const { ref, inView } = useInView({ triggerOnce: true, rootMargin: '150px 0px 20px' });
   return (
     <pre ref={ref} {...props}>
