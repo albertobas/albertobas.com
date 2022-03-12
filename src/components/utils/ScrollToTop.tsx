0e24ebad2a0164b290fe3436d0a26a9e55b78a19
Alberto <25537372+albertobas@users.noreply.github.com>
2022-03-12 22:47:33 +0100

    Edited onClick

--- src/components/utils/ScrollToTop.tsx
+++ src/components/utils/ScrollToTop.tsx
@@ -2,12 +2,14 @@ import SVG from './SVG';
 import styles from 'src/styles/modules/components/utils/scrolltotop.module.css';
 
 const ScrollToTop = ({ isVisible = true }: { isVisible?: boolean }) => {
-  const handleScrollUp = () => {
+  const handleScrollUp = (e: React.MouseEvent) => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
   return (
-    <div className={isVisible ? styles.container : styles.containerTransparent} onClick={handleScrollUp}>
-      <SVG icon="arrowUp" dim={{ width: 2, height: 2 }} />
+    <div className={isVisible ? styles.container : styles.containerTransparent}>
+      <a onClick={handleScrollUp} aria-label="Scroll to top">
+        <SVG icon="arrowUp" dim={{ width: 2.5, height: 2.5 }} />
+      </a>
     </div>
   );
 };
