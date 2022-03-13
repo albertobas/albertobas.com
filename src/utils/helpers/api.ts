f860ed3e98d2e0eb233b26d28b765077d9751f80
Alberto <25537372+albertobas@users.noreply.github.com>
2022-03-13 23:20:27 +0100

    Appended link to heading

--- src/utils/helpers/api.ts
+++ src/utils/helpers/api.ts
@@ -62,6 +62,7 @@ export const getMDX = (source: string) => {
             properties: {
               className: ['hashAnchor'],
             },
+            behavior: 'append',
           },
         ],
       ];
